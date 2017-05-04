3dd4210815d8f07a80f4d4d0e4adbe7ae4caa5d9
DaraChheang <dchhieng@zamanuniversity.edu.kh>
2017-05-04 08:43:59 +0700

    This is my official change of the Github project. I wrote "invertAllValue() "function. Does it right ?

--- Source.cpp
+++ Source.cpp
@@ -1,36 +1,53 @@
 #include <stdio.h>
 
-/*	
+/*
 *	Please define your functions after main().
 *	You can import standard C / C++ libraries.
 *	Recommended: add description of your code.
+ //I make some changes
 */
 
-	
-//this is testing 2nd;
-int getSum(int myArray[]);
-int getSumOfSquares(int myArray[]);
-void printAsChars(int myArray[]);
-double getAverage(int myArray[]);
+//int getSum(int myArray[]);
+//int getSumOfSquares(int myArray[]);
+//void printAsChars(int myArray[]);
+//double getAverage(int myArray[]);
 int subtractAllValues(int myArray[]);
-float invertAllValues(int myArray[] );
+//float invertAllValues(int myArray[] );
 void printGroupMemberNames();
 
 int main(void) {
 	printGroupMemberNames();
 
-	int row[500];
-	for (int i = 0; i < 500; i++)
+	int row[500], i;
+	for (i = 0; i < 500; i++)
 		row[i] = i;
+	subtractAllValues(row);
+
+
+
+	for (i = 0; i < 500; i++)
+		printf("%d", row[i]);
 
 	return 1;
+}
+
+int subtractAllValues(int myArray[])
+{
+	int i;
+	//printf("subtractAllValues = ");
+	for (i = 0; i<500; ++i) {
+		myArray[i] = -myArray[i];
+	}
 }
 
 
 
 
 void printGroupMemberNames() {
-	printf("James Bond\n Brad Pitt/n");
-
+	printf("Dara Chheang\n");
+	printf("Ratanaphea Chan\n");
+	printf("Chhorleang Horm\n");
+	printf("Sotheng Chheang\n");
+	printf("Savorn Chea\n");
+	printf("Madalen Iv\n");
 }
-
